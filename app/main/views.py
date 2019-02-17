cc35ddf7b5ed59cd890f4fd62beb5af5a51fb3bb
abby-shabi <abbyshabi@gmail.com>
2019-02-17 18:51:58 +0300

    added the subscribe method to the main/views file

--- app/main/views.py
+++ app/main/views.py
@@ -65,6 +65,8 @@ def new_blog(uname):
         posts = Post.query.all()
         return redirect(url_for('main.blog'))
     return render_template('new_blog.html', form = form, title =title)
+
+
 @main.route("/delete/<post_id>",methods = ['GET','POST'])
 @login_required
 def delete(post_id):
@@ -73,6 +75,7 @@ def delete(post_id):
     db.session.delete(post)
     db.session.commit()
     return redirect(url_for('main.blog'))
+
 
 @main.route("/update/<post_id>", methods= ['GET', 'POST'])
 @login_required
@@ -105,6 +108,7 @@ def profile(uname):
 
     return render_template("profile/profile.html", user = user,posts=posts)
 
+
 @main.route('/user/<uname>/update',methods = ['GET','POST'])
 @login_required
 def update_profile(uname):
@@ -124,6 +128,7 @@ def update_profile(uname):
 
     return render_template('profile/update.html',form = form)
 
+
 @main.route('/user/<uname>/update/pic',methods= ['POST'])
 @login_required
 def update_pic(uname):
@@ -134,5 +139,69 @@ def update_pic(uname):
         user.profile_pic_path = path
         db.session.commit()
     return redirect(url_for('main.profile',uname=uname))
+
+
+@main.route('/author/user/<uname>')
+def writer_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/author.html", user = user)
+
+
+@main.route('/subscribe',methods=["GET","POST"])
+def subscribe():
+    form=SubscribeForm()
+
+    if form.validate_on_submit():
+        subscriber = Subscribe(name=form.name.data,email=form.email.data)
+        db.session.add(subscriber)
+        db.session.commit()
+
+        mail_message("Welcome to Dee-blog","email/subscribe_user",subscriber.email,subscriber=subscriber)
+        
+        return redirect(url_for('main.blog'))
+        title = 'Subscribe'
+    return render_template('sub.html',subscribe_form=form)
+
+
+@main.route('/post/<post_id>/add/comment', methods = ['GET','POST'])
+def comment(post_id):
+  
+    post = Post.query.filter_by(id = post_id).first()
+    form = CommentForm()
+ 
+    if form.validate_on_submit():
+        body = form.body.data
+        author = form.author.data
+      
+        new_comment = Comment(body=body,author=author)
+        new_comment.save_comment()
+        
+        return redirect(url_for("main.read_post",post_id = post_id))
+    return render_template("comment.html", form = form, post = post)
+
+    
+@main.route('/<int:post_id>/comments')
+def show_comments(post_id):
+    
+    post = Post.query.filter_by(id = post_id).first()
+    
+    comments = Comment.get_comments(id)
+
+    return render_template('show_comments.html',comments= comments,post =post)
+    
+
+@main.route('/<int:post_id>/comments/delete')
+@login_required
+def delete_comment(post_id):
+    comment = Comment.query.filter_by(post_id = post_id).first()
+    post_id = comment.post.id
+   
+    db.session.delete(comment)
+    db.session.commit()
+    return redirect(url_for('main.show_comments',post_id = post_id))
 
        
