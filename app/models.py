cd8af74fedefa2f6793dcab7c919148d952bba63
abby-shabi <abbyshabi@gmail.com>
2019-02-17 19:07:33 +0300

    added get password method to models

--- app/models.py
+++ app/models.py
@@ -18,7 +18,7 @@ class Quote:
         self.id =id
         self.author = author
         self.quote = quote
-        
+
 class Role(db.Model):
     __tablename__ = 'roles'
 
@@ -29,3 +29,31 @@ class Role(db.Model):
 
     def __repr__(self):
         return f'User {self.name}'
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_hash = db.Column(db.String(255))
+
+
+
+    @property
+    def password(self):
+        raise AttributeError('You cannnot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+
+    def verify_password(self,password) :
+        return check_password_hash(self.password_hash,password)
+
+    def __repr__(self):
+        return f'User {self.username}'
