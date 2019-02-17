0ed850f53e9f2b851ec645a56dd9a441faf61760
abby-shabi <abbyshabi@gmail.com>
2019-02-17 18:47:46 +0300

    added new_blog method to main/views

--- app/main/views.py
+++ app/main/views.py
@@ -42,4 +42,27 @@ def read_post(post_id):
     
     return render_template("blog_page.html",post=post,comments= comments)
         
+@main.route('/new/blog/<uname>', methods = ['GET','POST'])
+@login_required
+def new_blog(uname):
+    form = PostForm()
+    title = 'Express yourself'
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+      
+    if form.validate_on_submit():
+        title = form.title.data
+        body = form.post.data
+        dateNow = datetime.datetime.now()
+        date = str(dateNow)
+        #author = User.query.filter_by(author_name = uname).first()
+
+
+        add_post = Post(title = title,body=body,date=date,user = current_user)
+        add_post.save_post()
+        posts = Post.query.all()
+        return redirect(url_for('main.blog'))
+    return render_template('new_blog.html', form = form, title =title)
         
