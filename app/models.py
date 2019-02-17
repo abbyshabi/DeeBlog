9764c30780d3a2998ef71da06fdc3e44d3d0c248
abby-shabi <abbyshabi@gmail.com>
2019-02-17 19:06:32 +0300

    added class Role

--- app/models.py
+++ app/models.py
@@ -18,3 +18,14 @@ class Quote:
         self.id =id
         self.author = author
         self.quote = quote
+        
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+
+
+    def __repr__(self):
+        return f'User {self.name}'
